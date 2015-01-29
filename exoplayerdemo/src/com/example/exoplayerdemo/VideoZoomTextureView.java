9ee146bdfb6ead80927eeddf406501d96f59f680
liz <qqli007009@gmail.com>
2015-01-29 11:40:34 +0800

    add videozoomtextureview for zoom,snapshot,move

---
+++ exoplayerdemo/src/com/example/exoplayerdemo/VideoZoomTextureView.java
@@ -0,0 +1,35 @@
+package com.example.exoplayerdemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by lz on 2015/1/29.
+ * <p/>
+ * DoWhat:
+ *
+ * refer to --- https://github.com/sephiroth74/ImageViewZoom
+ */
+
+public class VideoZoomTextureView extends VideoTextureView {
+
+
+    public VideoZoomTextureView(Context context) {
+        super(context);
+    }
+
+    public VideoZoomTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VideoZoomTextureView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+
+
+
+
+
+}
