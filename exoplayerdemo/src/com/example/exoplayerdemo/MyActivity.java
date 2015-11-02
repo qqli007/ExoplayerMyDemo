7b8a56cfe16121f2f0e2e0931562e05c4ca083f0
lz <lizheng@xunlei.com>
2015-11-02 17:24:30 +0800

    add test url

--- exoplayerdemo/src/com/example/exoplayerdemo/MyActivity.java
+++ exoplayerdemo/src/com/example/exoplayerdemo/MyActivity.java
@@ -8,6 +8,10 @@ import android.os.Environment;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MyActivity extends Activity {
 
     private TextView go;
@@ -27,8 +31,8 @@ public class MyActivity extends Activity {
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String url = Environment.getExternalStorageDirectory() + "/video.mp4";
-//                String url = "http://mvvideo2.meitudata.com/5450886dddd8e2048.mp4";
+//                String url = Environment.getExternalStorageDirectory() + "/video.mp4";
+                String url = getRandomUrl();
 
                 Intent to = new Intent(MyActivity.this, SimplePlayerActivity.class);
                 to.setData(Uri.parse(url));
@@ -36,6 +40,17 @@ public class MyActivity extends Activity {
 
             }
         });
+    }
+
+    private String getRandomUrl() {
+        List<String> data = new ArrayList<>();
+        data.add("http://7xl9af.com1.z0.glb.clouddn.com/011549bf539a672c2f245abe38d62a90d56c5f44");
+//        data.add("http://7xl9af.com1.z0.glb.clouddn.com/005567bf061b2b4bf3ad89a77aba06a9cdb367b6");
+        data.add("http://7xl9af.com1.z0.glb.clouddn.com/06ed123436d134c77ef11f815149c801597a221c");
+//        data.add("http://7xl9af.com1.z0.glb.clouddn.com/000f8d76c6d87bb61e61b145fa79b931bded20fb");
+
+        Random random = new Random();
+        return data.get(random.nextInt(data.size()));
     }
 
 
