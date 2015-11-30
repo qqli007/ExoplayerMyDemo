df587df007f7911f753c872b82ebe79666123d37
qqli007 <qqli007009@gmail.com>
2015-11-30 11:30:09 +0800

    Update MyActivity.java

--- exoplayerdemo/src/com/example/exoplayerdemo/MyActivity.java
+++ exoplayerdemo/src/com/example/exoplayerdemo/MyActivity.java
@@ -31,8 +31,7 @@ public class MyActivity extends Activity {
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                String url = Environment.getExternalStorageDirectory() + "/video.mp4";
-                String url = getRandomUrl();
+                String url = Environment.getExternalStorageDirectory() + "/video.mp4";
 
                 Intent to = new Intent(MyActivity.this, SimplePlayerActivity.class);
                 to.setData(Uri.parse(url));
@@ -40,17 +39,6 @@ public class MyActivity extends Activity {
 
             }
         });
-    }
-
-    private String getRandomUrl() {
-        List<String> data = new ArrayList<>();
-        data.add("http://7xl9af.com1.z0.glb.clouddn.com/011549bf539a672c2f245abe38d62a90d56c5f44");
-//        data.add("http://7xl9af.com1.z0.glb.clouddn.com/005567bf061b2b4bf3ad89a77aba06a9cdb367b6");
-        data.add("http://7xl9af.com1.z0.glb.clouddn.com/06ed123436d134c77ef11f815149c801597a221c");
-//        data.add("http://7xl9af.com1.z0.glb.clouddn.com/000f8d76c6d87bb61e61b145fa79b931bded20fb");
-
-        Random random = new Random();
-        return data.get(random.nextInt(data.size()));
     }
 
 
